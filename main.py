8e7b1481ff938ef00e7537d63548f93cf447e82a
gitwes <wesleyfreire1707@gmail.com>
2024-03-20 21:48:16 -0300

    Inicio

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-# É necessario intalar as lib
+# É necessario instalar as lib
 #pip install openpyxl pyautogui
 
 import openpyxl
 
+##para chamar a pasta 
 workbook = openpyxl.load_workbook('produtos_ficticios.xlsx')
+
+workbook['Produtos']
